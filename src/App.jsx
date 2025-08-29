7565aabca7abb640b830b994c0e8fc43b05320d3
Naresh Prajapati <prajapatinaresh2152006@gmail.com>
2025-08-29 12:23:44 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import AddNewDoctor from "./components/AddNewDoctor";
 import AddNewAdmin from "./components/AddNewAdmin";
 import Doctors from "./components/Doctors";
 import Messages from "./components/Messages";
+import ProtectedRoute from "./components/ProtectedRoute";
 import Sidebar from "./components/Sidebar";
 import { ToastContainer } from "react-toastify";
 import axios from "axios";
@@ -39,7 +40,15 @@ const App = () => {
       <Router>
         <Sidebar />
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          {/* <Route path="/" element={<Dashboard />} /> */}
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
           <Route path="/login" element={<Login />} />
           <Route path="/doctor/addnew" element={<AddNewDoctor />} />
           <Route path="/admin/addnew" element={<AddNewAdmin />} />
